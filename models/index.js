8d579a1865f2eb28fb23c8e84bbf880f0a06b82b
Charlie H <charltonhua@gmail.com>
2022-01-24 23:52:10 -0800

    13.4.4

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Post = require("./Post");
 const User = require("./User");
+const Vote = require("./Vote");
 
 // create associations
 
@@ -12,4 +13,35 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+// define a Many-to-Many juntion
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_post",
+  foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+
+// fk constraint -- create one-to-many associations directly between these models,
+// can perform aggreegated SQL functions between models
+Vote.hasMany(User, {
+  foreignKey: "user_id",
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(Vote, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Vote };
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post } = require("../../models");
 
 // GET /api/users
 router.get("/", (req, res) => {
@@ -22,6 +22,9 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
+    include: [
+      { model: Post, attributes: ["id", "title", "post_url", "created_at"] },
+    ],
   })
     .then((dbUserData) => {
       if (!dbUserData) {
