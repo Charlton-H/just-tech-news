1d803fc0bec78180c6f2de6d2dbecb69ed2d4cf0
Charlie H <charltonhua@gmail.com>
2022-02-05 10:41:20 -0800

    basic changes to try to fix heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+// setuo for use for cookies
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
